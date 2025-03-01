8c01c7b28fee4bdb7c371a8860c08b2a0e7e3544
ryanngu-afk <rngu82@gmail.com>
2025-03-01 02:21:19 -0800

    created login page

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
+});
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
