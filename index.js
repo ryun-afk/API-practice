4274ec242913c207b2e779188a09bee68ee71690
ryanngu-afk <rngu82@gmail.com>
2025-03-01 16:52:08 -0800

    added more static web pages

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/public/login.html');
 });
 
+app.get('/signup', (req, res) => {
+    res.sendFile(__dirname + '/public/signup.html');
+});
+
+app.get('/courses', (req, res) => {
+    res.sendFile(__dirname + '/public/courses.html');
+});
+
+app.get('/professors', (req, res) => {
+    res.sendFile(__dirname + '/public/professors.html');
+});
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
