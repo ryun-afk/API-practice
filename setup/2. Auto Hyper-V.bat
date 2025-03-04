c61e43c271c309b530b69b012c9953560de4f33e
ryanngu-afk <rngu82@gmail.com>
2025-03-04 03:29:12 -0800

    Create 2. Auto Hyper-V.bat

---
+++ setup/2. Auto Hyper-V.bat
@@ -0,0 +1,6 @@
+#https://superuser.com/questions/858259/hyper-v-reports-that-the-hypervisor-is-not-running-how-to-start-the-hypervisor
+#bcedit
+
+bcdedit /set hypervisorlaunchtype auto
+wsl --update
+wsl --set-default-version 2
