52177790043ea74fa5033b2d062a892ef50b4506
ryanngu-afk <rngu82@gmail.com>
2025-03-05 18:16:33 -0800

    Update index.js
    change comment

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 
-// middle-ware
+// middleware
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
