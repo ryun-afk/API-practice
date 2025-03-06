52a3ca6abef0787175bc83e28fd5ab647d781466
ryanngu-afk <rngu82@gmail.com>
2025-03-05 19:12:27 -0800

    adjusting controller.js
    ensure every controller and cases returns res.status.
    
    need to learn how to call getuserbyusername within adduser to reduce duplicate lines.

--- router/controller.js
+++ router/controller.js
@@ -4,7 +4,7 @@ const queries = require('./queries.js');
 const getUsers = (req, res) => {
     pool.query(queries.getUsers, (error, results) => {
         if (error) {throw error;}
-        res.status(200).json(results.rows);
+        return res.status(200).json(results.rows);
     });
 };
 
@@ -13,28 +13,41 @@ const getUserById = (req, res) => {
     pool.query(queries.getUserById, [user_id],(error, results) => {
         if (error) {throw error;}
         if (!results.rows.length===0){
-            res.status(404).json({message:'User not found'});
+            return res.status(404).json({message:'User not found'});
         }
-        res.status(200).json(results.rows);
+        return res.status(200).json(results.rows);
     });
 };
 
 const getUserByUsername = (req, res) => {
-    const user_id = parseInt(req.params.username);
+    const username = req.params.username;
     pool.query(queries.getUserByUsername, [username],(error, results) => {
         if (error) {throw error;}
-        if (!results.rows.length===0){
-            res.status(404).json({message:'User not found'});
+
+        if (results.rows.length===0){
+            return res.status(404).json({message:'User not found'});
         }
-        res.status(200).json(results.rows);
+        return res.status(200).json(results.rows);
     });
 };
 
 const addUser = (req, res) => {
     const {first_name, last_name, username, password} = req.body;
-    pool.query(queries.addUser, [first_name, last_name, username, password], (error, results) => {
+    
+    pool.query(queries.getUserByUsername, [username],(error, results) => {
         if (error) {throw error;}
-        res.status(201).json({message: 'User added to database'});
+
+        if (results.rows.length > 0) {
+            return res.status(409).json({ message: 'Username is taken' });
+        }
+
+        if (results.rows.length===0){
+            pool.query(queries.addUser, [first_name, last_name, username, password], (error, results) => {
+                if (error) {throw error;}
+        
+                return res.status(201).json({ message: 'User created successfully' });
+            });
+        }
     });
 };
 
@@ -43,27 +56,28 @@ const updateUser = (req, res)=> {
     const {first_name, last_name, username, password} = req.body;
     pool.query(queries.updateUser, [first_name, last_name, username, password, user_id], (error, results) => {
         if (error) {throw error;}
-        if (!results.rows.length===0){
-            res.status(404).json({message:'User not found'});
+
+        if (results.rows.length===0){
+            return res.status(404).json({message:'User not found'});
         }
-        res.status(200).json({message:'User updated successfully'});
+        return res.status(200).json({message:'User updated successfully'});
     });
 }
-
 
 const deleteUser = (req, res) => {
     const user_id = parseInt(req.params.user_id);
     pool.query(queries.deleteUser, [user_id],(error, results) => {
         if (error) {throw error;}
-        if (!results.rows.length===0){
-            res.status(404).json({message:'User not found'});
+        if (results.rows.length===0){
+            return res.status(404).json({message:'User not found'});
         }
-        res.status(200).json({message:'User deleted successfully'});
+        return res.status(200).json({message:'User deleted successfully'});
     });
 }
 
 const signupUser = (req, res) => {
     let {first_name, last_name, username, password} = req.body;
+
     pool.query(queries.getUserByUsername, [username], (error, results) => {
         if (error) {throw error;}
 
@@ -71,29 +85,18 @@ const signupUser = (req, res) => {
             res.status(409).json({message:'Username already exists'});
         }
         else{
-            addUser(req,res);
+            pool.query(queries.addUser, [first_name, last_name, username, password], (error, results) => {
+                if (error) {throw error;}
+                res.redirect('/login');
+            });
         }
-        
     });
 }
 
 
 const loginUser =(req,res) => {
-    let {username, password} = req.body;
-    pool.query(queries.getUserByUsername, [username], (error, results) => {
-        if (error) {throw error;}
-        if (results.rows.length===0){
-            res.status(404).json({message:'User not found'});
-        }
-        else{
-            if (results.rows[0].password===password){
-                res.status(200).json({message:'Login successful'});
-            }
-            else{
-                res.status(401).json({message:'Incorrect password'});
-            }
-        }
-    });
+    res.status(200).json({message:'Login successfully'});
+    
 }
 
 module.exports = {
