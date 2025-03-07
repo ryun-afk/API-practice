10de1075411c89f2db739c599701777b119273de
ryanngu-afk <rngu82@gmail.com>
2025-03-07 09:27:15 -0800

    renamed db.js and made its own folder

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 // Database connection setup
 const pool = new Pool({
-    user: process.env.DB_USER,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
 })
 
 module.exports = pool;
