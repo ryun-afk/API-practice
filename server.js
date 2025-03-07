69d4bdb8c70daf8a06cb906884cb6510b4dd319b
ryanngu-afk <rngu82@gmail.com>
2025-03-07 12:44:21 -0800

    changed index.js to server.js
    removed static file routes
    added sessions

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const route = require('./routes/routes.js');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Session setup
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, httpOnly: true, maxAge: 3600000 }
+}));
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Import routes
+app.use('/', route);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
