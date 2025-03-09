dbfa16a19fab7726a64f1ebbd7ee4a06c868654d
ryanngu-afk <rngu82@gmail.com>
2025-03-09 03:12:03 -0700

    deleting files
    deleted error-message css

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 // Middleware to check if the user is authenticated
 const isAuthenticated = (req, res, next) => {
     if (req.session.user) {
-        return next();  // User is authenticated, proceed to the next middleware or route
+        return next();
     }
     res.redirect('/login');
 };
