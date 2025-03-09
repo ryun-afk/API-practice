9d2a7d8a43bbe1336efddee8c8a31518e9517171
ryanngu-afk <rngu82@gmail.com>
2025-03-09 04:22:01 -0700

    removed express-validator
    express-validator was not being used
    
    it could be used to check for valid emails or password requirements.
    
    not using emails in this practice projects

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,31 +1,25 @@
 const bcrypt = require('bcryptjs');
-const {validationResult} = require('express-validator')
-
+const { body, validationResult } = require('express-validator');
 const pool = require('../config/dbconfig.js');
 const queries = require('../db/queries.js');
 
 // Login Controller
 const loginUser = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(400).json({
-            errors: errors.array()
-        });
-    }
-
-    const {username,password} = req.body;
     try {
+        // Validates if username is in database
+        const {username,password} = req.body;
         const result = await pool.query(queries.getUserByUsername,[username]);
         if (result.rows.length === 0) {
-            return res.render('login',{
+            return res.render('/login',{
                 message: 'Invalid credential'
             });
         }
 
+        // Validates if password matches
         const user = result.rows[0];
         const isPasswordValid = await bcrypt.compare(password,user.password);
         if (!isPasswordValid) {
-            return res.render('login',{
+            return res.render('/login',{
                 message: 'Invalid credential'
             });
         }
@@ -38,12 +32,10 @@ const loginUser = async (req, res) => {
             role: user.role,
         };
 
-        return res.render('dashboard',{
-            message: user.user_id
-        });
+        return res.redirect('/dashboard');
     } catch (error) {
         console.error('Login error: ', error);
-        return res.render('login',{
+        return res.render('/login',{
             message: 'Internal Server Error'
         });
     }
@@ -51,27 +43,18 @@ const loginUser = async (req, res) => {
 
 // Register Controller
 const registerUser = async (req,res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ 
-            errors: errors.array() 
-        });
-    }
-
-    const {first_name,last_name,username,password} = req.body;
     try {
+        const {username,password} = req.body;
         const existingUser = await pool.query(queries.getUserByUsername,[username]);
         if (existingUser.rows.length > 0) { 
-            return res.render('register',{
+            return res.render('/register',{
                 message:'Username is taken'
             });
         };
 
         const hashedPassword = await bcrypt.hash(password,10);
-        const result = await pool.query(queries.addUser,[first_name,last_name,username,hashedPassword]);
-        return res.render('login',{
-            message: 'Account registered'
-        });
+        pool.query(queries.addUser,[username,hashedPassword]);
+        return res.redirect('/login');
 
     } catch (error){
         console.error('Registration Error: ', error);
@@ -83,17 +66,24 @@ const registerUser = async (req,res) => {
 
 // Logout Controller
 const logoutUser = (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return res.status(500).json({
-                message: 'Error during logout'
+    try {
+        req.session.destroy((err) => {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error during logout'
+                });
+            }
+            return res.status(200).json({
+                message: 'Logged out successfully'
             });
-        }
-        return res.status(200).json({
-            message: 'Logged out successfully'
         });
-    });
-}
+    } catch {
+        console.error('Logout Error: ', error);
+        return res.status(401).json({
+            message: 'Internal Server Error'
+        });
+    }
+};
 
 module.exports = {
     loginUser,
