1568235076daced7e6029f7e778c38d974e88c27
ryanngu-afk <rngu82@gmail.com>
2025-03-14 01:50:17 -0700

    Changed sidebar
    uses default home and dashboard on sidebar
    after logging in, it adds more options on sidebar

--- routes/routes.js
+++ routes/routes.js
@@ -17,7 +17,7 @@ const {
 } = require('../middlewares/authMiddleware');
 
 // end-points
-router.get('/', (req, res) => {res.render('index', { isLoggedIn: req.isLoggedIn });});
+router.get('/', (req, res) => {res.render('index.ejs');});
 router.get('/register',notAuthenticated, (req, res) => {res.render('register',{message: ''});});
 router.get('/login',notAuthenticated, (req, res) => {res.render('login',{message: ''});});
 router.get('/dashboard',isAuthenticated, (req, res) => {res.render('dashboard',{user:req.session.user});});
@@ -27,18 +27,4 @@ router.get('/logout',logoutUser);
 router.post('/register',registerUser);
 router.post('/login',loginUser);
 
-
-
-
-
-/*
-router.get('/api/user', controller.getUsers);
-router.get('/api/user/user-id/:user_id',controller.getUserById);
-router.get('/api/user/username/:username',controller.getUserByUsername);
-router.post('/api/user',controller.addUser);
-router.put('/api/user/:user_id',controller.updateUser);
-router.delete('/api/:user_id',controller.deleteUser);
-
-router.get('/dashboard', controller.getDashboard);
-*/
 module.exports = router;
