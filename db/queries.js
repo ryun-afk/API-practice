edae6c680060c7e0e80ad062ed74bd692681c299
ryanngu-afk <rngu82@gmail.com>
2025-03-15 10:58:49 -0700

    remove student role in sidebar
    keeping admin to edit other users

--- db/queries.js
+++ db/queries.js
@@ -5,18 +5,17 @@ const addUser = 'INSERT INTO public.users (username, password) VALUES ($1, $2)';
 const updateFirstName = 'UPDATE public.users SET first_name = $1 WHERE user_id = $2';
 const updateLastName = 'UPDATE public.users SET last_name = $1 WHERE user_id = $2';
 const updatePassword = 'UPDATE public.users SET password = $1 WHERE user_id = $2'
+const updateRole = 'UPDATE public.users SET role = $1 WHERE user_id = $2'
 const deleteUser = 'DELETE FROM public.users WHERE user_id = $1';
 
 module.exports = {
     getUsers,
     getUserById,
     getUserByUsername,
-
     addUser,
-
     updateFirstName,
     updateLastName,
     updatePassword,
-    
+    updateRole,
     deleteUser,
 };
