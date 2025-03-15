a2cb6b66bc1f1279a1cd6599ab21f01377dc25cf
ryanngu-afk <rngu82@gmail.com>
2025-03-15 09:03:21 -0700

    merged sidebar partials into one

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 const { 
     loginUser, 
     registerUser, 
@@ -16,11 +17,16 @@ const {
     notAuthenticated,
 } = require('../middlewares/authMiddleware');
 
+// middleware to make 'user' available
+router.use((req, res, next) => {
+    res.locals.user = req.session.user || null;
+    next();
+});
+
 // end-points
 router.get('/', (req, res) => {res.render('index.ejs');});
 router.get('/register',notAuthenticated, (req, res) => {res.render('register',{message: ''});});
 router.get('/login',notAuthenticated, (req, res) => {res.render('login',{message: ''});});
-
 router.get('/dashboard',isAuthenticated, (req, res) => {res.render('dashboard',{user:req.session.user});});
 router.get('/settings',isAuthenticated, (req, res) => {res.render('settings',{message: ''});});
 router.get('/logout',logoutUser);
