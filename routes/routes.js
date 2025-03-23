dfc7426608f04a33d9dd7d687c44b81708b9812f
ryanngu-afk <rngu82@gmail.com>
2025-03-23 08:28:10 -0700

    added admin dashboard

--- routes/routes.js
+++ routes/routes.js
@@ -9,17 +9,21 @@ const { isAuthenticated, notAuthenticated,isAdmin } = require('../middlewares/au
 
 // end-points
 router.get('/', (req, res) => {res.render('index');});
+router.get('/logout',logoutUser);
+router.post('/register',registerUser);
+router.post('/login',loginUser);
+
 router.get('/login',notAuthenticated, (req, res) => {res.render('login',{message: ''});});
 router.get('/register',notAuthenticated, (req, res) => {res.render('register',{message: ''});});
-router.get('/logout',logoutUser);
-router.get('/admin', isAuthenticated,isAdmin,getAllUsers);
+
 router.get('/dashboard',isAuthenticated, (req, res) => {res.render('dashboard',{user:req.session.user});});
 router.get('/courses',isAuthenticated,getAllCourses);
 router.get('/forum',isAuthenticated, (req, res) => {res.render('forum',{user:req.session.user});});
 router.get('/settings',isAuthenticated, (req, res) => {res.render('settings',{message: ''});});
-
-router.post('/register',registerUser);
-router.post('/login',loginUser);
 router.post('/settings',isAuthenticated, updateUser);
+
+router.get('/admin', isAuthenticated,isAdmin,getAllUsers);
+
+
 
 module.exports = router;
